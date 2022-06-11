d2ac52b791d1eec36fcfd3e45c261ce83bbb9bc5
Sergei-Kovalhcuk <skovalcuk89@gmail.com>
2022-06-11 22:32:19 +0600

    hw2_recipes

--- test_Django/test_Django/urls.py
+++ test_Django/test_Django/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from app.views import home_view, time_view, workdir_view
+from recipes.views import recipes_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home_view, name='home'),
     path('current_time/', time_view, name='time'),
-    # path('posts/', posts_list, name='post'),
+    path('<str:dish>/', recipes_view, name='dish'),
     path('workdir/', workdir_view, name='workdir'),
 
 ]
